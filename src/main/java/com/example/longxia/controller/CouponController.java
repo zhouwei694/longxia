208f3f882e864f5536da875d3205066d53e885d0
zhouwei <492996379@qq.com>
2026-03-30 15:02:14 +0800

    增加权限

--- src/main/java/com/example/longxia/controller/CouponController.java
+++ src/main/java/com/example/longxia/controller/CouponController.java
@@ -1,9 +1,11 @@
 package com.example.longxia.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.example.longxia.annotation.AuthCheck;
 import com.example.longxia.common.BaseResponse;
 import com.example.longxia.common.ResultUtils;
 import com.example.longxia.constant.UserConstant;
+import com.example.longxia.exception.BusinessException;
 import com.example.longxia.exception.ErrorCode;
 import com.example.longxia.exception.ThrowUtils;
 import com.example.longxia.model.dto.coupon.CouponActivateRequest;
@@ -11,9 +13,13 @@ import com.example.longxia.model.dto.coupon.CouponGenerateRequest;
 import com.example.longxia.model.dto.coupon.CouponQueryRequest;
 import com.example.longxia.model.dto.coupon.CouponVerifyRequest;
 import com.example.longxia.model.entity.Coupon;
+import com.example.longxia.model.entity.User;
+import com.example.longxia.model.enums.CouponStatusEnum;
+import com.example.longxia.model.enums.UserRoleEnum;
 import com.example.longxia.model.vo.CouponStatisticsVO;
 import com.example.longxia.model.vo.CouponVO;
 import com.example.longxia.service.CouponService;
+import com.example.longxia.service.UserService;
 import com.mybatisflex.core.paginate.Page;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +28,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 卡券 控制层。
@@ -37,11 +47,13 @@ public class CouponController {
     @Autowired
     private CouponService couponService;
 
+    @Autowired
+    private UserService userService;
+
     /**
      * 卡券统计数据（管理员）
      */
     @GetMapping("/statistics")
-    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<CouponStatisticsVO> getCouponStatistics() {
         return ResultUtils.success(couponService.getCouponStatistics());
     }
@@ -60,19 +72,38 @@ public class CouponController {
     }
 
     /**
-     * 卡券列表（管理员）
+     * 卡券列表
      * 分页查询，支持按卡券号、卡券名称、卡券状态、核销人筛选
      */
     @PostMapping("/list/page")
-    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Page<CouponVO>> listCouponByPage(@RequestBody CouponQueryRequest couponQueryRequest) {
         ThrowUtils.throwIf(couponQueryRequest == null, ErrorCode.PARAMS_ERROR);
         long pageNum = couponQueryRequest.getPageNum();
         long pageSize = couponQueryRequest.getPageSize();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        // 当前登录用户
+        User loginUser = userService.getLoginUser(request);
+        // 获取当前用户具有的权限
+        UserRoleEnum userRoleEnum = UserRoleEnum.getEnumByValue(loginUser.getUserRole());
+        // 没有权限，拒绝
+        if (userRoleEnum == null) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+
         Page<Coupon> couponPage = couponService.page(Page.of(pageNum, pageSize),
                 couponService.getQueryWrapper(couponQueryRequest));
         Page<CouponVO> couponVOPage = new Page<>(pageNum, pageSize, couponPage.getTotalRow());
         List<CouponVO> couponVOList = couponService.getCouponVOList(couponPage.getRecords());
+        //权限是用户，并且在未激活，已激活状态下，密码变成******
+        if(UserRoleEnum.USER.equals(userRoleEnum) && !Objects.equals(CouponStatusEnum.VERIFIED.getValue(),couponQueryRequest.getStatus())){
+            couponVOList = couponVOList.stream().map(x -> {
+                CouponVO couponVO = new CouponVO();
+                BeanUtil.copyProperties(x, couponVO);
+                couponVO.setCouponPassword("******");
+                return couponVO;
+            }).toList();
+        }
         couponVOPage.setRecords(couponVOList);
         return ResultUtils.success(couponVOPage);
     }
@@ -94,7 +125,6 @@ public class CouponController {
      * 通过卡券密码核销，必须是已激活状态
      */
     @PostMapping("/verify")
-    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
     public BaseResponse<Boolean> verifyCoupon(@RequestBody CouponVerifyRequest couponVerifyRequest,
                                               HttpServletRequest request) {
         ThrowUtils.throwIf(couponVerifyRequest == null, ErrorCode.PARAMS_ERROR);
